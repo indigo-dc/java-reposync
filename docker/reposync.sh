a87f7c32f9633190300fce4d02df167993d3ee5f
jose-sanchezm <jose.sanchezm@atos.net>
2016-06-20 10:36:12 +0200

    Add integration test

--- docker/reposync.sh
+++ docker/reposync.sh
@@ -5,6 +5,10 @@ case $COMMAND in
     java -cp /lib/reposync.jar com.atos.indigo.reposync.Main -Djava.util.logging.config.file="$HOME/.indigo-reposync/reposync-log.properties"
     ;;
 
+  test)
+    java -cp /lib/reposync.jar com.atos.indigo.reposync.IntegrationTest -Djava.util.logging.config.file="$HOME/.indigo-reposync/reposync-log.properties"
+    ;;
+
   *)
     ARGS="$@";
     java -cp /lib/reposync.jar com.atos.indigo.reposync.ReposyncClient $@
